c3e0d319e672b31b095e26ea3b95ccf29babb218
aliali2131998 <40716889+aliali2131998@users.noreply.github.com>
2023-11-17 20:35:48 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
-const express = require('express');
-const app = express();
-const http = require('http');
-const server = http.createServer(app);
+var WebSocketServer = require("ws").Server
+var http = require("http")
+var express = require("express")
+var app = express()
+var port = process.env.PORT || 5000
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
-});
+app.use(express.static(__dirname + "/"))
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
-});
+var server = http.createServer(app)
+server.listen(port)
+
+console.log("http server listening on %d", port)
+
+var wss = new WebSocketServer({server: server})
+console.log("websocket server created")
+
+wss.on("connection", function(ws) {
+  var id = setInterval(function() {
+    ws.send(JSON.stringify(new Date()), function() {  })
+  }, 1000)
+
+  console.log("websocket connection open")
+
+  ws.on("close", function() {
+    console.log("websocket connection close")
+    clearInterval(id)
+  })
+})
