dd54f699b8272cf2ff9eedd9fc88f65b7e39921a
aliali2131998 <40716889+aliali2131998@users.noreply.github.com>
2023-11-18 09:18:45 +0400

    Update index.js

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ var wss = new WebSocketServer({server: server})
 console.log("websocket server created")
 
 wss.on("connection", function(ws) {
-  var id = setInterval(function() {
-    ws.send(JSON.stringify(new Date()), function() {  })
-  }, 1000)
+
+ws.send(JSON.stringify(new Date()), function() {  })
+
 
   console.log("websocket connection open")
 
